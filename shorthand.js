39350ef861dab790325c31524416348c5dcb3ad1
ahkamulbd <ahkamulbd@gmail.com>
2022-07-31 17:43:11 +0600

    Advanced Mathematicl operation shorthand shown according to Lecture 16-7

---
+++ shorthand.js
@@ -0,0 +1,35 @@
+// Variable Declaration:
+
+//var tomatoPrice = 25;
+
+// variable value update:
+
+// tomatoPrice = 30;
+
+// console.log(tomatoPrice); // Print : 30, not 25;
+
+// Add something to the previous value:
+
+// var price1 = 31;
+// var price2 = 20;
+// var price3 = 10;
+// var price4 = 100;
+// // add some value to the existing/current value
+// // price1 = price1 + 10;
+// price1 += 10; // += means adding some value with the existing value
+
+// price2 -= 5;
+// price3 *= 5;
+// price4 /= 5;
+
+// console.log(price1);    // Print: 41
+// console.log(price2);    // Print: 15
+// console.log(price3);    // Print: 50
+// console.log(price4);    // Print: 20
+
+// Adding 1 with the existing value:
+
+var age = 5;
+age++;
+
+console.log(age);
