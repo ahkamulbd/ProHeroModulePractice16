e2e2e2bfa1fa958ffd6263143d251656927ab9da
ahkamulbd <ahkamulbd@gmail.com>
2022-07-31 18:12:30 +0600

    String Concatenation with parseInt and parseFloat shown according to Lecture 16-8

---
+++ conversion.js
@@ -0,0 +1,27 @@
+// var fisrtName = 'John';
+// var lastName = 'Doe';
+// var fullName = fisrtName + ' ' + lastName;
+
+// console.log(fullName);
+
+// String Concatenation :
+
+// var price1 = '31';
+// var price2 = '10';
+// var totalPrice = price1 + price2;
+
+// //console.log(totalPrice);    // Print: 3110, not 41 because it is string concatenation
+
+// // Conversion String to Number:
+
+// price1 = parseInt(price1);
+// price2 = parseInt(price2);
+
+// totalPrice = price1 + price2;
+
+// console.log(totalPrice);    // Print : 41 
+
+var gpa = '3.45';
+var gpaNumber = parseFloat(gpa);
+
+console.log(gpaNumber);
