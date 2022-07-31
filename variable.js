cdaeac2b381827d18be1c7225ffac8fd9d5acbe6
ahkamulbd <ahkamulbd@gmail.com>
2022-07-31 12:26:41 +0600

    String & Boolean data types shown according to Lecture 16-4

--- variable.js
+++ variable.js
@@ -1,6 +1,21 @@
+// Numeric Data Type:
+
 var price = 11;
 var age = 17;
 var temp = 38;
 var weight = 80;
 var money = 5000;
 var child = 2;
+
+// String Data Type: j
+
+var person = "Sadar Uddin";
+var location = "TTDC Lama";
+var special = 'Sania Mirza';
+
+
+// Boolean Data Type:
+
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
