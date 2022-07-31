0dcb375c117f995a64a1c04be28a92f6c223209a
ahkamulbd <ahkamulbd@gmail.com>
2022-07-31 16:38:45 +0600

    Simple operation of addition, subtraction, multiplication and vision in JS shown according to Lecture 16-6

---
+++ operation.js
@@ -0,0 +1,36 @@
+// Addition in JS:
+
+// var onionPrice = 20;
+// var eggPrice = 10;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+
+// var totalPrice = onionPrice + eggPrice;
+
+// console.log(totalPrice);
+
+// // Subtraction in JS:
+
+// var priceDifference = onionPrice - eggPrice;
+
+// console.log(priceDifference);
+
+// Multiplication in JS:
+
+// var orangePrice = 20;
+// var quantity = 7;
+
+// var totalCost = orangePrice * quantity;
+
+// console.log(totalCost);
+
+// Division in JS:
+
+var money = 500;
+var Player = 10;
+
+var eachPlayer = money / Player;
+
+console.log(eachPlayer);
